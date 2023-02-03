e4ef70538e9d0928c3ab669536c4115427e587a4
hurrikaanig <hurrikaanig@users.noreply.github.com>
2023-02-03 12:23:39 +0000

    update html docs for ad21e1d

---
+++ implementors/core/ops/arith/trait.Sub.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"cairo_vm":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a>&lt;<a class=\"struct\" href=\"cairo_vm/vm/runners/cairo_runner/struct.ExecutionResources.html\" title=\"struct cairo_vm::vm::runners::cairo_runner::ExecutionResources\">ExecutionResources</a>&gt; for <a class=\"struct\" href=\"cairo_vm/vm/runners/cairo_runner/struct.ExecutionResources.html\" title=\"struct cairo_vm::vm::runners::cairo_runner::ExecutionResources\">ExecutionResources</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
